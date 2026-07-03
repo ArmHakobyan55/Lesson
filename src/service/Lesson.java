e041c608d1685542bbbfc29d8c91e4af9bd2715b
LIL19-IT <lilitdrnoyan30@gmail.com>
2026-07-03 11:38:27 +0400

    First commit

---
+++ src/service/Lesson.java
@@ -0,0 +1,50 @@
+package service;
+
+public class Lesson {
+    public static void main(String[] args) {
+
+//        byte x = 46;
+//        System.out.println(x);
+//        x = -20;
+//        System.out.println(x);
+//
+//        short y = 2000;
+//        System.out.println(y);
+//
+//        int  z = 124578;
+//        System.out.println(z);
+//
+//        long l = 12345678L;
+//        System.out.println(l);
+//
+//        float f = 123.67F;
+//        System.out.println(f);
+//
+//        double d = 24568.48;
+//        System.out.println(d);
+//
+//        boolean b = false;
+//        boolean bb = true;
+//
+//        System.out.println(b);
+//        System.out.println(bb);
+//
+//        char c = '\u3409';
+//        System.out.println(c);
+
+        String name = "name:                    Arman";
+        String surname = "surname:              Hakobyan";
+        String age = "age:                        26";
+        String address = "address:               Yerevan";
+        String email = "email: armhakobyan2000@gmail.com";
+
+        System.out.println("-----------Resume------------");
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(age);
+        System.out.println(address);
+        System.out.println(email);
+        System.out.println("-----------------------------");
+
+    }
+}
