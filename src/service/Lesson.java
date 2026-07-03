8408f0304183b3be60d1340a710ef320f3d6b58c
ArmHakobyan55 <armhakobyan@2000gmail.com>
2026-07-03 15:32:02 +0400

    Last commit

--- src/service/Lesson.java
+++ src/service/Lesson.java
@@ -2,49 +2,65 @@ package service;
 
 public class Lesson {
     public static void main(String[] args) {
+//         21harc
+//            int x = 36, y = 47, z = 27;
+//            if (x > y && x > z){
+//                System.out.println(x);
+//            } else if (y > x && y > z){
+//                System.out.println(y);
+//            }else{
+//                System.out.println(z);
+//            }
 
-//        byte x = 46;
-//        System.out.println(x);
-//        x = -20;
-//        System.out.println(x);
-//
-//        short y = 2000;
-//        System.out.println(y);
-//
-//        int  z = 124578;
-//        System.out.println(z);
-//
-//        long l = 12345678L;
-//        System.out.println(l);
-//
-//        float f = 123.67F;
-//        System.out.println(f);
-//
-//        double d = 24568.48;
-//        System.out.println(d);
-//
-//        boolean b = false;
-//        boolean bb = true;
-//
-//        System.out.println(b);
-//        System.out.println(bb);
-//
-//        char c = '\u3409';
-//        System.out.println(c);
+        //22 harc
+//          int x = 36, y = 47, z = 27;
+//            if (x < y && x < z){
+//                System.out.println(x);
+//            } else if (y < x && y < z){
+//                System.out.println(y);
+//            }else{
+//                System.out.println(z);
+//            }
+          //23 harc
+//        int x = 36, y = 47, z = 27;
+//        if (x == 1 ||  y == 1 || z == 1){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
+        //24 harc
+//        int x = 36, y = 47, z = 27;
+//        if ((x == 2 &&  y == 2) || (y == 2 && z == 2) || (x == 2 && z == 2)){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
 
-        String name = "name:                    Arman";
-        String surname = "surname:              Hakobyan";
-        String age = "age:                        26";
-        String address = "address:               Yerevan";
-        String email = "email: armhakobyan2000@gmail.com";
-
-        System.out.println("-----------Resume------------");
-        System.out.println(name);
-        System.out.println(surname);
-        System.out.println(age);
-        System.out.println(address);
-        System.out.println(email);
-        System.out.println("-----------------------------");
-        System.out.println("barev");
+        //25 harc
+//        int x = 36, y = 47, z = 27;
+//        if (x + y > z &&  y + z > x && x + z > y){
+//            System.out.println("y= 1");
+//        }else{
+//            System.out.println("y= 2");
+//        }
+        //26 harc
+//        int x = 3, y = 6, z = 9;
+//        if (z - y == y - x){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
+        //27 harc
+//        int x = 3, y = 9, z = 27;
+//        if (z / y == y / x){
+//            System.out.println(true);
+//        }else{
+//            System.out.println(false);
+//        }
+        //28 harc
+        int x = 6, y = 3, z = 14;
+        if ((x < y && x < z) && (y < x && y < z) && (z < x && z < y)){
+            System.out.println(x + " " + y + " " + z);
+        }
     }
 }
