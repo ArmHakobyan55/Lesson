a07dfafc0f79fff66e096fa7580d9ac91a53f417
ArmHakobyan55 <armhakobyan@2000gmail.com>
2026-07-17 01:28:28 +0400

    commit message

--- src/service/Text.java
+++ src/service/Text.java
@@ -13,7 +13,8 @@ public class Text {
 //            System.out.println();
 //        }
 
-        // 1 harc
+        // 1
+
 //        int[] numbers = new int[10];
 //
 //        for (inti = 0; i < numbers.length; i++) {
@@ -23,7 +24,8 @@ public class Text {
 //        for (inti = 0; i < numbers.length; i++) {
 //            System.out.print(numbers[i] + " ");
 //        }
-        // 2 harc
+        // 2
+
 //        int[] numbers = new int[1000];
 //        for (inti = 1; i < numbers.length; i++) {
 //            numbers[i] = i + 1;
@@ -33,7 +35,7 @@ public class Text {
 //            System.out.println(numbers[i]);
 //        }
 
-        //3 harc
+        //3
 
 //        int[] numbers = new int[20];
 //        int index = 0;
@@ -43,7 +45,8 @@ public class Text {
 //        for (inti = 0; i < numbers.length; i++){
 //            System.out.println(numbers[i]);
 //        }
-        //4 harc
+        //4
+
 //        int[] numbers = {10, 6, 8, -20, 14};
 //
 //        for (inti = 0; i < numbers.length; i++) {
@@ -51,7 +54,8 @@ public class Text {
 //                System.out.println(numbers[i]);
 //            }
 //        }
-        //5 harc
+        //5
+
 //        float[] numbers = new float[468];
 //        int index = 0;
 //        for (inti = 0; i < 468; i++) {
@@ -61,7 +65,8 @@ public class Text {
 //                System.out.println(i);
 //            }
 //        }
-        //6 harc
+
+        //6
 //        int n = 7;
 //        int [] table = new int[10];
 //        int index = 0;
@@ -70,5 +75,24 @@ public class Text {
 //            System.out.println(n + " * " + i + " = " + n * i);
 //
 //        }
+//        int n = 9;
+//        for (inti = 1; i <= 10 ; i++) {
+//            System.out.println(n + " * " + i + " = " + n * i);
+//
+//        }
+//        int [] numbers1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+//        int [] numbers2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20};
+//        int sum1 = 0;
+//        int sum2 = 0;
+//        for (inti = 0; i < numbers1.length; i++) {
+//             sum1 += numbers1[i];
+//        }
+//
+//        for (inti = 0; i < numbers2.length; i++){
+//             sum2 += numbers2[i];
+//        }
+//
+//        System.out.println(sum1 - sum2);
+//    }
     }
 }
---
+++ src/service/Text1.java
@@ -0,0 +1,242 @@
+package service;
+
+public class Text1 {
+    public static void main(String[] args) {
+        //211 harc
+//        int[] numbers = {4, -7, 8, -15, 9, 2, -5};
+//
+//        int sum = 0;
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] > 0) {
+//                sum += numbers[i];
+//                count++;
+//            }
+//        }
+//        double arithmetic_mean = sum / count;
+//        System.out.println(arithmetic_mean);
+        //axper es im mot 5.0a tpum bayc chhaskaca inchi
+        //214 harc
+
+//        int[] numbers = {4, -7, 8, -15, 9, 2, -5};
+//
+//        int sum = 0;
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] < 0) {
+//                sum += numbers[i];
+//                count++;
+//            }
+//        }
+//        double arithmetic_mean = sum / count;
+//        System.out.println(arithmetic_mean);
+        //215 harc
+
+//        int[] numbers = {4, -7, 8, -15, 9, 2, -5};
+//        int sum = 0;
+//        for (inti = 0; i < numbers.length; i++){
+//            if(i % 2 == 0){
+//                sum += numbers[i];
+//            }
+//        }
+//        System.out.println(sum);
+        //216 harc
+
+//        int[] numbers = {4, -7, 8, -15, 9, 2, -5};
+//
+//        int product = 1;
+//        for (inti = 0; i < numbers.length; i++){
+//            if(i % 2 == 0){
+//                product *= numbers[i];
+//            }
+//        }
+//        System.out.println(product);
+
+        //219 harc
+
+//        int[] numbers = {4, -7, 8, -15, 9, 2, -5, 7, 16};
+//
+//        int k = 2;
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++){
+//            if(i % k == 0){
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+        //221 harc
+//        int[] numbers = {4, 7, 8, 15, 9, 2, 5, 7, 16};
+//        int sum = 0;
+//        for (inti = 0; i < numbers.length; i++){
+//            if (numbers[i] >= 4 && numbers[i] <= 9){
+//                sum += numbers[i];
+//            }
+//        }
+//        System.out.println(sum);
+        //222 harc
+
+//        int[] numbers = {4, 7, 8, 15, 9, 2, 5, 7, 16};
+//        int product = 1;
+//        for (inti = 0; i < numbers.length; i++){
+//            if (numbers[i] >= 4 && numbers[i] < 7){
+//                product *= numbers[i];
+//            }
+//        }
+//        System.out.println(product);
+        //223 harc
+
+//        int[] numbers = {4, 7, 8, 15, 9, 2, 5, 7, 16};
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++){
+//            if (numbers[i] > 4 && numbers[i] < 15){
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+        //227 harc
+
+//        int[] numbers = {4, 7, 8, 1, 9, 2, 5, 7, 16, 3};
+//
+//        int k = 3;
+//        int sum = 0;
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++){
+//            if (i % k == 0){
+//                sum += numbers[i];
+//                count++;
+//            }
+//        }
+//        double arithmetic_mean = sum / count;
+//        System.out.println(arithmetic_mean);
+
+        // 228 harc
+
+//        int[] numbers = {4, 7, 8, 1, 9, 2, 5, 7, 16, 3};
+//
+//        int k = 3;
+//        int sum = 0;
+//        for (inti = 0; i < numbers.length; i++){
+//            if (i % k == 0){
+//                sum += numbers[i];
+//            }
+//        }
+//        System.out.println(sum);
+
+        //229 harc
+//
+//        int[] numbers = {2, -7, -8, 4, -9,};
+//
+//        int difference = 1;
+//        for (inti = 0; i < numbers.length; i++){
+//            if (numbers[i] - i > 0){
+//                difference *= numbers[i];
+//            }
+//        }
+//        System.out.println(difference);
+
+        //231 harc
+//        int[] numbers = {2, 7, 4, 6, 9};
+//
+//        int sum = 0;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] % 2 == 0) {
+//                sum += numbers[i] * numbers[i];
+//            }
+//        }
+//
+//        System.out.println(sum);
+
+        //232 harc
+
+//        int[] numbers = {2, 8, 4, 6, 9};
+//
+//        int counte = 0;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] % 2 == 0) {
+//                counte++;
+//            }
+//        }
+//        System.out.println(counte);
+        //233 harc
+
+//        int[] numbers = {2, 5, 4, 6, 9};
+//
+//        int sum = 0;
+//        int product = 1;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] % 2 == 0) {
+//                sum += numbers[i];
+//                product *= numbers[i];
+//            }
+//        }
+//        System.out.println(sum);
+//        System.out.println(product);
+
+        //234 harc
+//        int[] numbers = {2, 5, 4, 6, 9};
+//
+//        int sum = 0;
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] % 2 == 1) {
+//                sum += numbers[i];
+//                count++;
+//            }
+//        }
+//        double arithmetic_mean = sum / count;
+//        System.out.println(arithmetic_mean);
+        //236 harc
+
+//        int[] numbers = {2, 5, 4, 6, 9};
+
+//        int product = 1;
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] % 2 == 1) {
+//                product *= numbers[i];
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+//        System.out.println(product);
+
+        //237 harc
+//        int[] numbers = {2, 5, 0, 4, 6, 0, 9, 0};
+//
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] == 0) {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+        //238 harc
+//        int[] numbers = {2, 5, 4, 6, 9, 12};
+//
+//        int k = 3;
+//        int sum = 0;
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] % k == 0) {
+//                sum += numbers[i];
+//                count++;
+//            }
+//        }
+//        double arithmetic_mean = sum / count;
+//        System.out.println(arithmetic_mean);
+
+        //240 harc
+//        int[] numbers = {2, 7, 4, 28, 9, 12};
+//
+//        int count = 0;
+//        for (inti = 0; i < numbers.length; i++) {
+//            if (numbers[i] % 7 == 0) {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+    }
+}
